9b1d336132b19c16bcfff721e27a49353c26e6d9
MatobushkaVlad <VDChuprakov@stud.kantiana.ru>
2022-03-30 18:14:20 +0300

    Создан каркас стурктуры

--- main.cpp
+++ main.cpp
@@ -1,7 +1,26 @@
 #include <iostream>
 
+struct Tree
+{
+	int data;
+	Tree* left;
+	Tree* right;
+	Tree* parent;
+};
+
+Tree* Make(int data, Tree* p)
+{
+
+}
+
+void Add(int data, Tree*& root)
+{
+
+}
+
+
 int main()
 {
-	
+
 	return 0;
 }
