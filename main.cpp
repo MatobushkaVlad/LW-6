f36185cfeadfd46bf9ec63cdd2da3665261a6787
MatobushkaVlad <VDChuprakov@stud.kantiana.ru>
2022-03-30 18:17:46 +0300

    Реализация создания дерева и добавления элемента в него

--- main.cpp
+++ main.cpp
@@ -10,17 +10,40 @@ struct Tree
 
 Tree* Make(int data, Tree* p)
 {
-
+	Tree* q = new Tree;
+	q->data = data;
+	q->left = nullptr;
+	q->right = nullptr;
+	q->parent = p;
+	return q;
 }
 
 void Add(int data, Tree*& root)
 {
-
+	if (root == nullptr)
+	{
+		root = Make(data, nullptr);
+		return;
+	}
+	Tree* v = root;
+	while ((data < v->data && v->left != nullptr) || (data > v->data && v->right != nullptr))
+		if (data < v->data)
+			v = v->left;
+		else
+			v = v->right;
+	if (data == v->data)
+		return;
+	Tree* u = Make(data, v);
+	if (data < v->data)
+		v->left = u;
+	else
+		v->right = u;
 }
 
 
 int main()
 {
+	Tree* root = nullptr;
 
 	return 0;
 }
